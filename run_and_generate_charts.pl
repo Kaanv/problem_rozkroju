c3220fce5f740671b1034a47479abaf1f978a287
Pawel Ficek <ficek.pawel@gmail.com>
2016-06-07 20:21:56 +0200

    Prosty skrypt uruchamiający

---
+++ run_and_generate_charts.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub open_file
+{
+	my $file = $_[0];
+
+	open (FH, "< $file") or die "Can't open $file for read: $!";
+	my @lines = <FH>;
+	close FH or die "Cannot close $file: $!"; 
+
+	return @lines;
+}
+
+my @file = open_file($ARGV[0]);
+my $output;
+
+my $max_search = 10000;
+my $max_neigh = 1;
+
+my $current_data = "$max_search $max_neigh ";
+
+my $name;
+
+for (my $i = 0; $i < @file; $i++)
+{
+	if ($file[$i] =~ /('.*')\r?\n/)
+	{
+		if ($current_data ne "$max_search $max_neigh ")
+		{
+			$output = `./problem $current_data`;
+			$current_data = "$max_search $max_neigh ";
+			print $name, ": $output";
+		}
+
+		$name = $1;
+		next;
+	}
+	while ($file[$i] =~ /([0-9\.]+)/g)
+	{
+		$current_data = $current_data . $1 . " ";
+	}
+}
+
+$output = `./problem $current_data`;
+print "$name", ": $output";
+
